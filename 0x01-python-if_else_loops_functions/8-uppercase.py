52ffa4804eb1e182e2ee7cb03fd35709fd62111a
julietowah <104780614+julietowah@users.noreply.github.com>
2022-08-23 21:05:57 -0700

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhon
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
