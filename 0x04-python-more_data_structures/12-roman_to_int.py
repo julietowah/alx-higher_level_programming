b1d0d38473b32b66c018d86d44f1269e65d7a64d
julietowah <104780614+julietowah@users.noreply.github.com>
2022-08-31 16:13:39 -0700

    update 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+    result = 0
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    s = list(roman_string)
+    for x in range(len(s)):
+        array = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        number = [1, 5, 10, 50, 100, 500, 1000]
+        for i in range(len(array)):
+            if (s[x] == array[i]):
+                s[x] = number[i]
+                break
+    if len(s) == 1:
+        return (s[0])
+    else:
+        for x in range(1, len(s)):
+            if s[x - 1] >= s[x]:
+                result = result + s[x - 1]
+            else:
+                result = result - s[x - 1]
+        result = result + s[x]
+        return result
