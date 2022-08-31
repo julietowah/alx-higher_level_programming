c8e5c272a2c17e25d62ff967032c9cd1149fac5c
julietowah <104780614+julietowah@users.noreply.github.com>
2022-08-31 16:11:09 -0700

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
-
 def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return 
+    uniq_list = set(my_list)
+    sum = 0
+
+    for i in uniq_list:
+        sum += i
+
+    return (sum)
