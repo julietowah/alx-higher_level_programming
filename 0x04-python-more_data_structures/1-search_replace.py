3718090f1b369f8a6ab7202e4c90e2ff8ccd8c49
julietowah <104780614+julietowah@users.noreply.github.com>
2022-08-31 15:54:09 -0700

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
-  
+    new_list = my_list.copy()
+    count = 0
+    if my_list is None:
+        return ()
+    for i in new_list:
+        if i == search:
+            new_list[count] = replace
+        count = count + 1
+    return (new_list)
